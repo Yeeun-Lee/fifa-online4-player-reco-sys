b1d6020574baceed4862926979060ac9e6bb9a49
Yeeun-Lee <yeeun3430@gmail.com>
2020-07-02 18:21:16 +0900

    bot commit

--- slack_bot/command.py
+++ slack_bot/command.py
@@ -1,10 +1,4 @@
-import random
-import time
-import pandas as pd
-import numpy as np
-
 from slackclient import SlackClient
-# 추천 엔진
 
 # util
 TOKEN = "xoxb-1224553073220-1203698095863-Au6LzjuGW79YvY1NhibkNDNn"
